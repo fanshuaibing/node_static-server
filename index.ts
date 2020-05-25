7f734ccd39495e1b7f5c8328d27baba6f07c595b
fatfan <fatfan1020@gmail.com>
2020-05-25 23:11:18 +0800

    feat: 处理不存在的文件
    返回一个 404 页面

--- index.ts
+++ index.ts
@@ -12,8 +12,18 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   const filename = pathname.substr(1);
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
-      response.statusCode = 404;
-      response.end("no  such file or dir");
+      if (error.errno === -4058) {
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, "404.html"), (error, data) => {
+          response.end(data);
+        });
+      } else if (error.errno === -4068) {
+        response.statusCode = 403;
+        response.end("无权查看目录内容");
+      } else {
+        response.statusCode = 500;
+        response.end("服务器繁忙，请稍后重试");
+      }
     } else {
       response.end(data);
     }
